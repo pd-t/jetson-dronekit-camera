6b4ac017f4b462d66a5a6d87fd52cb3259ac784d
thomasf <tf@wogra.com>
2021-05-31 10:09:59 +0200

    simplified setup, removed dockerized sitl sincce no improvement for unit-test

--- app/example.py
+++ app/example.py
@@ -2,11 +2,16 @@ from time import sleep
 
 from dronekit import connect
 
+import dronekit_sitl
+
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
+
 print("Connecting")
-vehicle = connect('127.0.0.1:14551', wait_ready=True)
+vehicle = connect(connection_string, wait_ready=True)
 
 
-@vehicle.on_message('CAMERA')
+@vehicle.on_message('*')
 def listener(self, name, message):
     print('message: %s' % message)
 
